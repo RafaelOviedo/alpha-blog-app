16a532e343858c22e591d0dfc858e66cf7958d6e
Rafael Oviedo <rafaeloviedo@Rafaels-MBP.fibertel.com.ar>
2022-12-14 23:38:18 -0300

    update(*): adding signup form

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root 'pages#home'
   resources :articles
+  get 'signup', to: 'users#new'
+  
+  post 'users', to: 'users#create'
+  resources :users, except: [ :new ]
 end
